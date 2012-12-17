e328ab763acb1c08867859666fc6fa8465f83cf4
Mark McGuire <mark.b.mcg@gmail.com>
2012-12-17 18:52:11 +0000

    Move app context setup to __init__ in BaseNamespace

--- lobbypy/namespaces/base.py
+++ lobbypy/namespaces/base.py
@@ -2,12 +2,14 @@ from socketio.namespace import BaseNamespace as Namespace
 from lobbypy import app
 
 class BaseNamespace(Namespace):
-    def initialize(self):
-        request = self.request
-        self.ctx = app.request_context(request.environ)
-        self.ctx.push()
-        app.preprocess_request()
-        self.request = self.ctx.request
+    def __init__(self, *args, **kwargs):
+        request = kwargs.get('request', None)
+        super(BaseNamespace, self).__init__(*args, **kwargs)
+        if request:
+            self.ctx = app.request_context(request.environ)
+            self.ctx.push()
+            app.preprocess_request()
+            self.request = self.ctx.request
 
     def disconnect(self, *args, **kwargs):
         super(BaseNamespace, self).disconnect(*args, **kwargs)
