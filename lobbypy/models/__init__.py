0db769436111fba19bb19b5194cc2de74f344c44
Mark McGuire <mark.b.mcg@gmail.com>
2012-12-22 03:43:02 +0000

    add spectator table to models __init__

--- lobbypy/models/__init__.py
+++ lobbypy/models/__init__.py
@@ -1,10 +1,11 @@
 from .player import Player
-from .lobby import Lobby, Team, LobbyPlayer
+from .lobby import Lobby, Team, LobbyPlayer, spectator_table
 from .utils import make_lobby_item_dict, make_lobby_dict
 
 __all__ = [
         'Player',
         'Lobby',
+        'spectator_table',
         'Team',
         'LobbyPlayer',
         'make_lobby_item_dict',
