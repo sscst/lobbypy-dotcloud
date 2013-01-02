16028df5c49e3be7f82711801a438cb037c49fa7
Mark McGuire <mark.b.mcg@gmail.com>
2013-01-02 18:23:02 +0000

    Add caching to steam_api

--- lobbypy/lib/steam_api.py
+++ lobbypy/lib/steam_api.py
@@ -1,5 +1,5 @@
 import os, json, urllib2
-
+from lobbypy.utils import cache
 def _get_api_key():
     return os.environ['STEAM_API_KEY']
 
@@ -16,6 +16,7 @@ def _get_player_friends_link_base():
             'GetFriendList/v0001/?key=%s&steamid=' %
             _get_api_key())
 
+@cache.memoize(15*60)
 def get_player_summary_for_steam_id(steam_id):
     link = '%s%s' % (_get_player_summaries_link_base(), steam_id)
     players = json.load(urllib2.urlopen(link))['response']['players']
@@ -23,6 +24,7 @@ def get_player_summary_for_steam_id(steam_id):
         raise LookupError('Error getting player summary from Steam API')
     return players[0]
 
+@cache.memoize(60*60)
 def get_player_friends_for_steam_id(steam_id):
     return json.load(urllib2.urlopen('%s%s' %
         (_get_player_friends_link_base(), steamid)))
