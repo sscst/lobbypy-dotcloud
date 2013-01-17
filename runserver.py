a12c35af33679e83baabb08e66d3ec1244dec166
Mark McGuire <mark.b.mcg@gmail.com>
2013-01-17 05:26:06 +0000

    only xhr-polling

--- runserver.py
+++ runserver.py
@@ -22,7 +22,7 @@ def run(debug, rcon_check):
     sys.stderr.write(title + '\n')
     config_app(app, DEBUG=debug, RCON_CHECK_SERVER=(not rcon_check))
     SocketIOServer(('', port), app, resource="socket.io", transports=lambda:
-            ['websocket', 'xhr-polling']).serve_forever()
+            ['xhr-polling']).serve_forever()
 
 @manager.command
 def init_db():
